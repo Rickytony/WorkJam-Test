a7208a89f71dba5c7f85629dbd78c682cbaa8c56
Richard <karp.richard@gmailcom>
2019-02-10 22:19:19 -0500

    adds header component

---
+++ app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary border-0 text-white">
+      <div className="container">
+        <h1>WorkJam -- HackerNews API Test</h1>
+      </div>
+    </nav>
+  );
+};
+
+export { Header };
--- app/main.js
+++ app/main.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { StoriesList } from "./components/StoriesList.jsx";
+import { Header } from "./components/Header.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Main = () => {
-  return <StoriesList />;
+  return (
+    <React.Fragment>
+      <Header />
+      <StoriesList />
+    </React.Fragment>
+  );
 };
 
 ReactDOM.render(<Main />, document.getElementById("main"));
